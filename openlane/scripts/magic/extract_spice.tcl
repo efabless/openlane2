fafd11956e5ccfc587ae5fe4e0c2e4e9360ea793
Uri Shaked <uri@urishaked.com>
2023-07-13 21:40:17 +0300

    Extract shorts as resistors (during magic SPICE extraction)
    This fixes LVS failures when more than two pins are connected to the same net. See #111 for details.

--- openlane/scripts/magic/extract_spice.tcl
+++ openlane/scripts/magic/extract_spice.tcl
@@ -38,6 +38,7 @@ if { ! $::env(MAGIC_NO_EXT_UNIQUE) } {
 extract
 
 ext2spice lvs
+ext2spice short resistor
 ext2spice -o $netlist $::env(DESIGN_NAME).ext
 feedback save $feedback_file
 # exec cp $::env(DESIGN_NAME).spice $::env(signoff_results)/$::env(DESIGN_NAME).spice
