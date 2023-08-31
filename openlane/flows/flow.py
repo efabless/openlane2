292283269187d55ebb6379ccbd6d02a25ac3feb7
Donn <me@donn.website>
2023-08-31 18:43:20 +0300

    Fix Test

--- openlane/flows/flow.py
+++ openlane/flows/flow.py
@@ -423,8 +423,6 @@ class Flow(ABC):
 
         # Stored until next start()
         self.run_dir = os.path.join(self.design_dir, "runs", tag)
-        # Stored until next start()
-        self.toolbox = Toolbox(os.path.join(self.run_dir, "tmp"))
 
         initial_state = with_initial_state or State()
 
@@ -473,6 +471,9 @@ class Flow(ABC):
         except FileNotFoundError:
             info(f"Starting a new run of the '{self.name}' flow with the tag '{tag}'.")
             mkdirp(self.run_dir)
+
+        # Stored until next start()
+        self.toolbox = Toolbox(os.path.join(self.run_dir, "tmp"))
 
         warning_log_path = os.path.join(self.run_dir, "warnings.log")
         warning_handler = logging.FileHandler(warning_log_path, mode="a+")
