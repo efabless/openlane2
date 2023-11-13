4d4b85e56f672aa1dc8fa61b62309fc57d79997a
Mohamed Gaber <donn@efabless.com>
2023-11-13 13:15:21 +0200

    Update Verilator
    * Also change from a derivation to a simple override

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -17,34 +17,34 @@ let
     fetchTarball "https://github.com/NixOS/nixpkgs/archive/3b79cc4bcd9c09b5aa68ea1957c25e437dc6bc58.tar.gz"
     ) {};
 in args: import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0218941ea68b4c625533bead7bbb94ccce52dceb.tar.gz") {
-    overlays = [
-        (new: old: {
-            # aarch64-related
-            clp = old.clp.overrideAttrs(finalAttrs: previousAttrs: {
-                meta = {
-                    platforms = previousAttrs.meta.platforms ++ ["aarch64-linux"];
-                };
-            });
+  overlays = [
+    (new: old: {
+      # aarch64-related
+      clp = old.clp.overrideAttrs(finalAttrs: previousAttrs: {
+        meta = {
+            platforms = previousAttrs.meta.platforms ++ ["aarch64-linux"];
+        };
+      });
 
-            # Darwin-related
-            lemon-graph = old.lemon-graph.overrideAttrs (finalAttrs: previousAttrs: {
-                meta = { broken = false; };
-                doCheck = false;
-            });
-            
-            or-tools = old.or-tools.overrideAttrs (finalAttrs: previousAttrs: {
-                meta = {
-                    platforms = previousAttrs.meta.platforms ++ old.lib.platforms.darwin;
-                };
-                doCheck = false;
-            });
+      # Darwin-related
+      lemon-graph = old.lemon-graph.overrideAttrs (finalAttrs: previousAttrs: {
+        meta = { broken = false; };
+        doCheck = false;
+      });
+      
+      or-tools = old.or-tools.overrideAttrs (finalAttrs: previousAttrs: {
+        meta = {
+            platforms = previousAttrs.meta.platforms ++ old.lib.platforms.darwin;
+        };
+        doCheck = false;
+      });
 
-            # # Hack to get GHDL to maybe work on macOS- use at your own risk
-            # ghdl-llvm = if old.stdenv.isDarwin then newpkgs.ghdl-llvm.overrideAttrs (finalAttrs: previousAttrs: {
-            #     meta = {
-            #         platforms = previousAttrs.meta.platforms ++ old.lib.platforms.darwin;
-            #     };
-            # }) else (old.ghdl-llvm);
-        })
-    ];
+      # # Hack to get GHDL to maybe work on macOS- use at your own risk
+      # ghdl-llvm = if old.stdenv.isDarwin then newpkgs.ghdl-llvm.overrideAttrs (finalAttrs: previousAttrs: {
+      #     meta = {
+      #         platforms = previousAttrs.meta.platforms ++ old.lib.platforms.darwin;
+      #     };
+      # }) else (old.ghdl-llvm);
+    })
+  ];
 } // args
