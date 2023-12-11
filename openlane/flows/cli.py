c66647e8b73d0ec09c2b76b1e322e9b7921f53a8
kareefardi <kareem.farid@efabless.com>
2023-12-11 18:44:39 +0200

    Revert "scl"
    This reverts commit 48daee251a40ddc2e2d38ae070cecd2eecd90f8e.

--- openlane/flows/cli.py
+++ openlane/flows/cli.py
@@ -140,7 +140,7 @@ def pdk_scl_cb(
 
             pdk_root = volare.get_volare_home(values["pdk_root"])
 
-            volare.enable(pdk_root, pdk_family, get_opdks_rev(), include_libraries=[values["scl"]])
+            volare.enable(pdk_root, pdk_family, get_opdks_rev())
     return value
 
 
