4a6de69128c89117ce20c7c5bdd6f362e0237e7e
Mohamed Gaber <donn@efabless.com>
2023-12-14 13:31:52 +0200

    Fix report

--- openlane/scripts/openroad/irdrop.tcl
+++ openlane/scripts/openroad/irdrop.tcl
@@ -21,33 +21,31 @@ source $::env(SCRIPTS_DIR)/openroad/common/set_rc.tcl
 read_spef $::env(CURRENT_SPEF_DEFAULT_CORNER)
 
 if { [info exists ::env(VSRC_LOC_FILES)] } {
+    puts "%OL_CREATE_REPORT irdrop.rpt"
     foreach {net vsrc_file} "$::env(VSRC_LOC_FILES)" {
         set arg_list [list]
         lappend arg_list -net $net
         lappend arg_list -outfile $::env(STEP_DIR)/net-$net.csv
         lappend arg_list -vsrc $vsrc_file
-        puts "%OL_CREATE_REPORT irdrop.rpt"
         analyze_power_grid {*}$arg_list
-        puts "%OL_END_REPORT"
     }
+    puts "%OL_END_REPORT"
 } else {
-    puts "\[INFO] Using voltage extracted from lib ($::env(LIB_VOLTAGE)) for power nets and 0V for ground nets…"
+    puts "\[INFO] Using voltage extracted from lib ($::env(LIB_VOLTAGE)V) for power nets and 0V for ground nets…"
+    puts "%OL_CREATE_REPORT irdrop.rpt"
     foreach net "$::env(VDD_NETS)" {
         set arg_list [list]
         lappend arg_list -net $net
         lappend arg_list -outfile $::env(STEP_DIR)/net-$net.csv
         set_pdnsim_net_voltage -net $net -voltage $::env(LIB_VOLTAGE)
-        puts "%OL_CREATE_REPORT irdrop.rpt"
         analyze_power_grid {*}$arg_list
-        puts "%OL_END_REPORT"
     }
     foreach net "$::env(GND_NETS)" {
         set arg_list [list]
         lappend arg_list -net $net
         lappend arg_list -outfile $::env(STEP_DIR)/net-$net.csv
         set_pdnsim_net_voltage -net $net -voltage 0
-        puts "%OL_CREATE_REPORT irdrop.rpt"
         analyze_power_grid {*}$arg_list
-        puts "%OL_END_REPORT"
     }
+    puts "%OL_END_REPORT"
 }
