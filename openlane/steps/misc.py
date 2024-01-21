e99bc19282d3e653a6555b507345983e4055437a
Kareem Farid <kareefardi@users.noreply.github.com>
2024-01-21 13:26:52 +0200

    remove unused module

--- openlane/steps/misc.py
+++ openlane/steps/misc.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-import textwrap
 from typing import Tuple
 
 from .step import ViewsUpdate, MetricsUpdate, Step
