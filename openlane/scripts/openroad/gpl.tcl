373b9b0831afc078c1650b073b663374faf4eea3
Mohamed Gaber <donn@efabless.com>
2024-01-23 16:27:46 +0300

    Fix Merge Fallout
    * Updated submodule
    * Fixed renamed variable not being fixed in script

--- openlane/scripts/openroad/gpl.tcl
+++ openlane/scripts/openroad/gpl.tcl
@@ -63,7 +63,7 @@ set cell_pad_side [expr $::env(GPL_CELL_PADDING) / 2]
 
 lappend arg_list -pad_right $cell_pad_side
 lappend arg_list -pad_left $cell_pad_side
-lappend arg_list -init_wirelength_coef $::env(PL_WIRELENGTH_COEF)
+lappend arg_list -init_wirelength_coef $::env(PL_WIRE_LENGTH_COEF)
 
 puts "{*}$arg_list"
 global_placement {*}$arg_list
