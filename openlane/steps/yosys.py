6dc349ab140e63214c57e432a716cb58755aa4d0
Mohamed Gaber <donn@efabless.com>
2024-03-07 23:42:04 +0200

    Fix VHDLSynthesis

--- openlane/steps/yosys.py
+++ openlane/steps/yosys.py
@@ -96,7 +96,7 @@ def _generate_read_deps(
     commands = "set ::_synlig_defines [list]\n"
 
     synth_defines = [f"PDK_{config['PDK']}", f"SCL_{config['STD_CELL_LIBRARY']}\""]
-    synth_defines += config["VERILOG_DEFINES"] or []
+    synth_defines += config.get("VERILOG_DEFINES", []) # VERILOG_DEFINES not defined for VHDLSynthesis
     for define in synth_defines:
         commands += f"verilog_defines {TclUtils.escape(f'-D{define}')}\n"
         commands += (
@@ -106,7 +106,7 @@ def _generate_read_deps(
     scl_lib_list = toolbox.filter_views(config, config["LIB"])
 
     if power_defines:
-        if power_define := config.get("VERILOG_POWER_DEFINE"):
+        if power_define := config.get("VERILOG_POWER_DEFINE"): # VERILOG_POWER_DEFINE not defined for VHDLSynthesis
             commands += f"verilog_defines {TclUtils.escape(f'-D{power_define}')}\n"
             commands += f"lappend ::_synlig_defines {TclUtils.escape(f'+define+{power_define}')}\n"
 
