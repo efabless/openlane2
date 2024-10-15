3a40a81750b8a3d14da93aadf12fe92522e22af7
Mohamed Gaber <donn@efabless.com>
2024-10-15 11:13:34 +0300

    typo

--- nix/create-shell.nix
+++ nix/create-shell.nix
@@ -32,7 +32,7 @@
   openlane-env = (
     python3.withPackages (pp:
       with pp;
-        if include-openlane then openlane else openlane.propagatedBuildInputs
+        if include-openlane then [openlane] else openlane.propagatedBuildInputs
         ++ extra-python-packages
         ++ openlane-plugins)
   );
