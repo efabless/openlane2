24cbd263b487f2c7a3cf4cf0c55b1fdfa9dbb537
Sylvain Munaut <tnt@246tNt.com>
2024-10-29 09:06:21 +0000

    steps/openroad: Add WriteCDL step
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- openlane/state/design_format.py
+++ openlane/state/design_format.py
@@ -224,6 +224,13 @@ DesignFormat(
 ).register()
 
 DesignFormat(
+    "cdl",
+    "cdl",
+    "Circuit Design Language",
+    alts=["CDL"],
+).register()
+
+DesignFormat(
     "gds",
     "gds",
     "GDSII Stream",
