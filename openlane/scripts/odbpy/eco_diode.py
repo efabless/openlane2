c81df2f8fe5d13dabad24d621d9e1f0e66ebb8de
Mohamed Gaber <donn@efabless.com>
2025-02-26 15:22:44 +0200

    Bump version, fix copyright years

--- openlane/scripts/odbpy/eco_diode.py
+++ openlane/scripts/odbpy/eco_diode.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2020 Efabless Corporation
+# Copyright 2025 Efabless Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
